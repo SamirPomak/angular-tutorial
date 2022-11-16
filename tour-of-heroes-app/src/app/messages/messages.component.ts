720876f8a296be0a0f9e2331cc3a78b001c847d4
Samir Pomak <samir.pomak@tick42.com>
2022-11-16 17:33:44 +0200

    add message component

---
+++ tour-of-heroes-app/src/app/messages/messages.component.ts
@@ -0,0 +1,13 @@
+import { MessageService } from './../message.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss'],
+})
+export class MessagesComponent implements OnInit {
+  constructor(public messageService: MessageService) {}
+
+  ngOnInit(): void {}
+}
